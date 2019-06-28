f4614ae335ee3002b4dd03dab92f6959f6e40c14
Jacob Qian <jacob.qian@digipen.edu>
2019-06-28 14:45:24 -0700

    Player movement in enclosed space

---
+++ centipede/Assets/PlayerContoller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    public float speed = .05f;
+
+ 
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Rigidbody2D rb = GetComponent < Rigidbody2D>();
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += Vector2.left * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += Vector2.right * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += Vector2.up * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity += Vector2.down * speed;
+        }
+
+        
+    
+        
+ 
+    }
+}
