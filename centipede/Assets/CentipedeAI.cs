daec97f70ecf82f9d86b9b3b9bf1bb0a8d5d5e89
Peter Lee <peter.lee@digipen.edu>
2019-06-28 15:13:06 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ centipede/Assets/CentipedeAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentipedeAI : MonoBehaviour
+{
+    public Vector2Int position;
+    List<Vector2> lastPositions = new List<Vector2>();
+    public GameObject[] bodyparts;
+    public float speed;
+
+    void HeadMove()
+    {
+        lastPositions[0] = transform.position;
+        transform.position += new Vector3(speed, 0);
+    }
+
+
+    void MoveSegment(int currentSegment)
+    {
+        lastPositions[currentSegment + 1] = bodyparts[currentSegment].transform.position;
+        bodyparts[currentSegment].transform.position = lastPositions[currentSegment];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < bodyparts.Length + 1; i++)
+        {
+            lastPositions.Add(Vector2.zero);
+        }
+    }
+
+    void Move(Vector2Int newPosition)
+    {
+        lastPositions.Add(position);
+        position = newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        HeadMove();
+        for(int i = 0; i < bodyparts.Length; i++)
+        {
+            MoveSegment(i);
+        }
+        
+
+
+        //transform.position = new Vector3(position.x * scale, position.y * scale, 0);
+
+    }
+}
---
+++ centipede/Assets/Collision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<CentipedeAI>();
+        transform.position += new Vector3(0, -speed);
+}
