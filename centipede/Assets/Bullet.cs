18f7b66f7a1660e20618d3066c2fb60dfb22d83d
Casper <casper.wories@digipen.edu>
2019-06-28 15:13:58 -0700

    collidingcentipede

---
+++ centipede/Assets/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float speed = 1;
+
+    void Update()
+    {
+        transform.position += Vector3.up * speed;
+    }
+}
