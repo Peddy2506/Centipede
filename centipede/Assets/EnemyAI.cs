3da95ae328fb3e8244ef40f30e45240821a59100
Jacob <jacob.qian@digipen.edu>
2019-07-02 13:38:19 -0700

    Spider Jump V2

--- centipede/Assets/EnemyAI.cs
+++ centipede/Assets/EnemyAI.cs
@@ -8,45 +8,19 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Vector2 velocity;
-    public Vector2 acceleration;
-    Vector2 gravity = new Vector2(0, -1);
+    Rigidbody2D rb;
+    public float speed = 2;
+
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity += Time.deltaTime * (acceleration + gravity);
-        Vector3 p = transform.position;
-        // Crtl + D to d
-        p.x += Time.deltaTime * velocity.x;
-        p.y += Time.deltaTime * velocity.y;
-        transform.position = p;
-    }
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        int count = 0;
-        Vector2 sum = new Vector2(0, 0);
-        while (count < collision.contactCount)
-        {
-            sum += collision.contacts[count].point;
-            count += 1;
-        }
-        Vector2 average = sum / count;
-        Vector2 position = new Vector2(transform.position.x, transform.position.y);
-        Vector2 collisionPoint = average - position;
-        if (Mathf.Abs(collisionPoint.x) > Mathf.Abs(collisionPoint.y))
-        {
-            velocity.x *= -1;
-        }
-        else
-        {
-            velocity.y *= -1;
-        }
+        rb.velocity = new Vector3(speed, rb.velocity.y, 0);
     }
 }
 
